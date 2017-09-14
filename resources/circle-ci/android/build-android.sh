e3931ff194f9d901a7998214acdb6c25d5536d24
timkim <timk@adobe.com>
2017-09-14 15:37:21 -0700

    trying to fix plugin issues on ci

--- resources/circle-ci/android/build-android.sh
+++ resources/circle-ci/android/build-android.sh
@@ -3,6 +3,7 @@
 if [ "$(git rev-parse HEAD)" != "$(git rev-parse $(git describe --abbrev=0 --tags))" ]; then
     echo "Building adhoc release"
     npm run adhoc-android
+    cordova plugin add https://github.com/timkim/wikitude-cordova-plugin.git
 else
     echo "Building Appstore release"
     npm run release-android
