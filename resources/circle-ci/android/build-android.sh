aff8f927d8a3720179bfeed3631f5fae992658b9
timkim <timk@adobe.com>
2017-09-18 14:18:30 -0700

    Trying to make it work

--- resources/circle-ci/android/build-android.sh
+++ resources/circle-ci/android/build-android.sh
@@ -3,7 +3,6 @@
 if [ "$(git rev-parse HEAD)" != "$(git rev-parse $(git describe --abbrev=0 --tags))" ]; then
     echo "Building adhoc release"
     npm run adhoc-android
-    cordova plugin add https://github.com/timkim/wikitude-cordova-plugin.git --nofetch
 else
     echo "Building Appstore release"
     npm run release-android
